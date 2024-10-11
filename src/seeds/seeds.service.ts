a63c5513e96e964b515d89fa85c8c5aa3551a8c1
ZufarovAbdulla <zufarovabdulla08@gmail.com>
2024-10-12 01:13:11 +0500

    data added in seed service

--- src/seeds/seeds.service.ts
+++ src/seeds/seeds.service.ts
@@ -1,25 +1,28 @@
-// import { User, UserRoles } from "@modules";
+import { User, UserRoles } from "@modules";
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 
 @Injectable()
 export class SeedsService  implements OnModuleInit {
-    // constructor(@InjectModel(User) private userModel: typeof User) {}
+    constructor(@InjectModel(User) private userModel: typeof User) {}
 
     async onModuleInit() {
         await this.seedUsers()
     }
 
     async seedUsers() : Promise<void> {
-        // const usersCount = await this.userModel.count()
+        const usersCount = await this.userModel.count()
 
-        // if(usersCount == 0) {
-        //     await this.userModel.create({
-        //         name: "Abrorbek Abdulxamidov",
-        //         email: "abdulkhamidovabrorbek@gmail.com",
-        //         phone: "+998939386462",
-        //         role: UserRoles.admin
-        //     })
-        // }
+        if(usersCount == 0) {
+            await this.userModel.create({
+                id: 1,
+                first_name: "Abdulla",
+                last_name: "Zufarov",
+                username: "abdulla_zufarov",
+                email: "zufarovabdulla@gmail.com",
+                password: "$2b$12$Co1KFMw7VTIG/WuRrHaE7eFGqVmhW6ZcU7KE4T8XZAy2RspGPSDfS",
+                role: UserRoles.admin
+              })
+        }
     }
 }
